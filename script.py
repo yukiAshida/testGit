f25462237eac673c39218aff4ecf66b2156d336c
yukiAshida <ashidayuki@lelab.t.u-tokyo.ac.jp>
2019-11-19 02:22:43 +0900

    add axis information

--- script.py
+++ script.py
@@ -8,6 +8,13 @@ y = np.sin(x)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
+
+ax.set_title("sin wave")
+ax.set_xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_xlim(0, 2*np.pi)
+ax.set_ylim(-1,1)
+
 ax.plot(x, y)
 plt.show()
 
