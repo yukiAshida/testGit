2ab008dec419c5dc24b880825a04ea376584b2ae
yukiAshida <ashidayuki@lelab.t.u-tokyo.ac.jp>
2019-11-19 02:23:50 +0900

    add z

--- script.py
+++ script.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 2*np.pi, 1000)
 y = np.sin(x)
 
+z = np.cos(x)
 
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+ax = fig.add_subplot(1,2,1)
 ax.plot(x, y)
+ax = fig.add_subplot(1,2,2)
+ax.plot(x, z)
 plt.show()
 
